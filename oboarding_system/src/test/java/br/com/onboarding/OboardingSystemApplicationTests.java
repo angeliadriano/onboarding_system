192fba9642ef3ec5f7c6ffa422382718e6b7aa54
Adriano Angeli <adrianoangeli01@gmail.com>
2022-04-11 12:02:33 -0300

    First commit

---
+++ oboarding_system/src/test/java/br/com/onboarding/OboardingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OboardingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
